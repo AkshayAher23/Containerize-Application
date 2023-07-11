538ad9c0096be1ff7bc7da77abcf6a4e0dc64dec
Akshay Aher <akshay.aher@atyeti.com>
2023-07-11 17:01:08 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/ContainrizedAppllicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainrizedAppllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainrizedAppllicationApplication.class, args);
+	}
+
+}
